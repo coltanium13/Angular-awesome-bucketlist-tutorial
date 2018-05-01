09ef6fa4209b55d9819a09f0336768d51df0c628
Colton Gardner <ColtonBootCamp@DESKTOP-BBU8CVU>
2018-04-30 17:41:34 -0700

    adding list model and bucketlist controller as well as mongo promise for bluebird

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const config = require('./config/database');
 const bucketlist = require('./controllers/bucketlist');
 
 //Connect mongoose to our database
+mongoose.Promise = require('bluebird');
 mongoose.connect(config.database, { useMongoClient: true });
 
 //Declaring Port
