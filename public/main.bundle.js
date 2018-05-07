20d1fdb320d2e0e57bc7eeadc5e87a4dcd7ee06d
Colton Gardner <ColtonBootCamp@DESKTOP-BBU8CVU>
2018-05-06 17:36:50 -0700

    removing header stuff not needed anymore

---
+++ public/main.bundle.js
@@ -0,0 +1,381 @@
+webpackJsonp(["main"],{
+
+/***/ "./src/$$_lazy_route_resource lazy recursive":
+/***/ (function(module, exports) {
+
+function webpackEmptyAsyncContext(req) {
+	// Here Promise.resolve().then() is used instead of new Promise() to prevent
+	// uncatched exception popping up in devtools
+	return Promise.resolve().then(function() {
+		throw new Error("Cannot find module '" + req + "'.");
+	});
+}
+webpackEmptyAsyncContext.keys = function() { return []; };
+webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
+module.exports = webpackEmptyAsyncContext;
+webpackEmptyAsyncContext.id = "./src/$$_lazy_route_resource lazy recursive";
+
+/***/ }),
+
+/***/ "./src/app/add-list/add-list.component.css":
+/***/ (function(module, exports) {
+
+module.exports = ".push-button-down {\r\n  margin-top: 5%;\r\n}\r\n"
+
+/***/ }),
+
+/***/ "./src/app/add-list/add-list.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"container\">\n\n  <form (ngSubmit)=\"onSubmit()\">\n    <div>\n      <label for=\"title\">Title</label>\n      <input type=\"text\" [(ngModel)]=\"newList.title\" name=\"title\" required>\n    </div>\n\n    <div>\n      <label for=\"category\">Select Category</label>\n      <select [(ngModel)]=\"newList.category\" name = \"category\" >\n\n        <option value=\"High\">High Priority</option>\n        <option value=\"Medium\">Medium Priority</option>\n        <option value=\"Low\">Low Prioirty</option>\n\n      </select>\n    </div>\n\n    <div>\n      <label for=\"description\">description</label>\n      <input type=\"text\" [(ngModel)]=\"newList.description\" name=\"description\" required>\n    </div>\n\n    <button type=\"submit\">Submit</button>\n\n  </form>\n</div>\n"
+
+/***/ }),
+
+/***/ "./src/app/add-list/add-list.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AddListComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__services_list_service__ = __webpack_require__("./src/app/services/list.service.ts");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+var AddListComponent = /** @class */ (function () {
+    function AddListComponent(listServ) {
+        this.listServ = listServ;
+        this.addList = new __WEBPACK_IMPORTED_MODULE_0__angular_core__["t" /* EventEmitter */]();
+    }
+    AddListComponent.prototype.ngOnInit = function () {
+        this.resetNewList();
+    };
+    AddListComponent.prototype.resetNewList = function () {
+        this.newList = {
+            title: '',
+            category: '',
+            description: '',
+            _id: ''
+        };
+    };
+    AddListComponent.prototype.onSubmit = function () {
+        var _this = this;
+        this.listServ.addList(this.newList).subscribe(function (result) {
+            if (result.error || null) {
+                // do something with the error
+            }
+            else {
+                _this.addList.emit(_this.newList);
+                _this.resetNewList();
+            }
+        });
+    };
+    __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["K" /* Output */])(),
+        __metadata("design:type", __WEBPACK_IMPORTED_MODULE_0__angular_core__["t" /* EventEmitter */])
+    ], AddListComponent.prototype, "addList", void 0);
+    AddListComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
+            selector: 'app-add-list',
+            template: __webpack_require__("./src/app/add-list/add-list.component.html"),
+            styles: [__webpack_require__("./src/app/add-list/add-list.component.css")]
+        }),
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1__services_list_service__["a" /* ListService */]])
+    ], AddListComponent);
+    return AddListComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/app.component.css":
+/***/ (function(module, exports) {
+
+module.exports = ""
+
+/***/ }),
+
+/***/ "./src/app/app.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<div style=\"text-align:center\">\n  <h1>\n    {{title}}!\n  </h1>\n\n  <app-view-list> </app-view-list>\n\n</div>\n\n"
+
+/***/ }),
+
+/***/ "./src/app/app.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var AppComponent = /** @class */ (function () {
+    function AppComponent() {
+        this.title = 'app';
+    }
+    AppComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
+            selector: 'app-root',
+            template: __webpack_require__("./src/app/app.component.html"),
+            styles: [__webpack_require__("./src/app/app.component.css")]
+        })
+    ], AppComponent);
+    return AppComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/app.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__("./node_modules/@angular/platform-browser/esm5/platform-browser.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_common_http__ = __webpack_require__("./node_modules/@angular/common/esm5/http.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_forms__ = __webpack_require__("./node_modules/@angular/forms/esm5/forms.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__app_component__ = __webpack_require__("./src/app/app.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__add_list_add_list_component__ = __webpack_require__("./src/app/add-list/add-list.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__view_list_view_list_component__ = __webpack_require__("./src/app/view-list/view-list.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__services_list_service__ = __webpack_require__("./src/app/services/list.service.ts");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+
+
+
+
+var AppModule = /** @class */ (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["E" /* NgModule */])({
+            declarations: [
+                __WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */],
+                __WEBPACK_IMPORTED_MODULE_5__add_list_add_list_component__["a" /* AddListComponent */],
+                __WEBPACK_IMPORTED_MODULE_6__view_list_view_list_component__["a" /* ViewListComponent */]
+            ],
+            imports: [
+                __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+                __WEBPACK_IMPORTED_MODULE_2__angular_common_http__["b" /* HttpClientModule */],
+                __WEBPACK_IMPORTED_MODULE_3__angular_forms__["a" /* FormsModule */]
+            ],
+            providers: [__WEBPACK_IMPORTED_MODULE_7__services_list_service__["a" /* ListService */]],
+            bootstrap: [__WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */]]
+        })
+    ], AppModule);
+    return AppModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/services/list.service.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return ListService; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_common_http__ = __webpack_require__("./node_modules/@angular/common/esm5/http.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_rxjs_add_operator_map__ = __webpack_require__("./node_modules/rxjs/_esm5/add/operator/map.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+var httpOptions = {
+    headers: new __WEBPACK_IMPORTED_MODULE_1__angular_common_http__["c" /* HttpHeaders */]({ 'Content-Type': 'application/json' })
+};
+var ListService = /** @class */ (function () {
+    function ListService(httpclient) {
+        this.httpclient = httpclient;
+        this.serverApi = 'http://localhost:3000';
+    }
+    ListService.prototype.getAllLists = function () {
+        var URI = this.serverApi + "/bucketlist/";
+        return this.httpclient.get(URI);
+    };
+    ListService.prototype.deleteList = function (listId) {
+        var URI = this.serverApi + "/bucketlist/" + listId;
+        return this.httpclient.delete(URI, httpOptions);
+    };
+    ListService.prototype.addList = function (list) {
+        var URI = this.serverApi + "/bucketlist/";
+        var body = JSON.stringify({ title: list.title, description: list.description, category: list.category });
+        return this.httpclient.post(URI, body, httpOptions);
+    };
+    ListService = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["w" /* Injectable */])(),
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1__angular_common_http__["a" /* HttpClient */]])
+    ], ListService);
+    return ListService;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/view-list/view-list.component.css":
+/***/ (function(module, exports) {
+
+module.exports = ".btn-danger {\r\n  color: #eeeeee;\r\n\r\n}\r\n\r\n.align-center {\r\n  text-align: center;\r\n}\r\n"
+
+/***/ }),
+
+/***/ "./src/app/view-list/view-list.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<h2> Awesome Bucketlist App11 </h2>\r\n\r\n<!-- Table starts here -->\r\n<table id=\"table\">\r\n  <thead>\r\n  <tr>\r\n    <th>Priority Level</th>\r\n    <th>Title</th>\r\n    <th>Description</th>\r\n    <th> Delete </th>\r\n\r\n  </tr>\r\n  </thead>\r\n  <tbody>\r\n  <tr *ngFor=\"let list of lists\">\r\n    <td>{{list.category}}</td>\r\n    <td>{{list.title}}</td>\r\n    <td>{{list.description}}</td>\r\n    <td> <button type=\"button\" (click)=\"deleteList(list); $event.stopPropagation();\">Delete</button></td>\r\n\r\n  </tr>\r\n  </tbody>\r\n</table>\r\n\r\n<app-add-list (addList)='onAddList($event)'> </app-add-list>\r\n\r\n"
+
+/***/ }),
+
+/***/ "./src/app/view-list/view-list.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return ViewListComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__services_list_service__ = __webpack_require__("./src/app/services/list.service.ts");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+var ViewListComponent = /** @class */ (function () {
+    function ViewListComponent(listServ) {
+        this.listServ = listServ;
+        //lists propoerty which is an array of List type
+        this.lists = [];
+    }
+    ViewListComponent.prototype.ngOnInit = function () {
+        //Load all list on init
+        this.loadLists();
+    };
+    ViewListComponent.prototype.loadLists = function () {
+        var _this = this;
+        //Get all lists from server and update the lists property
+        this.listServ.getAllLists().subscribe(function (result) {
+            if (result.error || null) {
+                // do something with error here
+            }
+            else {
+                _this.lists = result.data;
+            }
+        });
+    };
+    //The deleted list is being filtered out using the .filter method
+    ViewListComponent.prototype.deleteList = function (list) {
+        var _this = this;
+        this.listServ.deleteList(list._id).subscribe(function (result) {
+            if (result.error || null) {
+                // do something with error here
+            }
+            else {
+                _this.lists = _this.lists.filter(function (lists) { return lists !== list; });
+            }
+        });
+        //response =>	this.lists = this.lists.filter(lists => lists !== list),)
+    };
+    ViewListComponent.prototype.onAddList = function (newList) {
+        this.lists = this.lists.concat(newList);
+    };
+    ViewListComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
+            selector: 'app-view-list',
+            template: __webpack_require__("./src/app/view-list/view-list.component.html"),
+            styles: [__webpack_require__("./src/app/view-list/view-list.component.css")]
+        }),
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1__services_list_service__["a" /* ListService */]])
+    ], ViewListComponent);
+    return ViewListComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/environments/environment.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+var environment = {
+    production: false
+};
+
+
+/***/ }),
+
+/***/ "./src/main.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__ = __webpack_require__("./node_modules/@angular/platform-browser-dynamic/esm5/platform-browser-dynamic.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_app_module__ = __webpack_require__("./src/app/app.module.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__("./src/environments/environment.ts");
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_7" /* enableProdMode */])();
+}
+Object(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */])
+    .catch(function (err) { return console.log(err); });
+
+
+/***/ }),
+
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__("./src/main.ts");
+
+
+/***/ })
+
+},[0]);
+//# sourceMappingURL=main.bundle.js.map
